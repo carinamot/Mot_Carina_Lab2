53475cb89cbb222e9310d0d9b003767a1680cd26
carina_mot <carina_mot@yahoo.com>
2020-10-28 20:54:13 +0200

    Laborator 4

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace Mot_Carina_Lab2.Models
 {
@@ -10,7 +10,9 @@ namespace Mot_Carina_Lab2.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         public ICollection<Order> Orders { get; set; }
+        public ICollection<PublishedBook> PublishedBooks { get; set; }
     }
 }
